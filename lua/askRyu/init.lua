01689017da50062690b25f4548fb198aedbc065c
Gesty Linaga <83747778+gestylinaga@users.noreply.github.com>
2024-01-18 01:48:19 -0800

    feat: add nvim-notify dependency for nice popup display

--- lua/askRyu/init.lua
+++ lua/askRyu/init.lua
@@ -1,10 +1,20 @@
 local quotes = require("askRyu.quotes")
-
+local notify = require("notify")
 local M = {}
 
+local opts = {
+  ---------------------------------------------
+  -- see nvim-notify docs for full opts list --
+  ---------------------------------------------
+  title = "Ryu says...",
+  icon = " 🥋", -- other icons to use { "🥋", "💨", "✊", "💪", "🦵" }
+  render = "default", -- other render opts { "minimal", "simple", "compact" }
+  timeout = 2500, -- in ms (default = 5000)
+}
+
 M.askRyu = function()
-  local quote = quotes.getQuote()
-  print("Ryu says 💪🥋: " .. "\"" .. quote .. "\"")
+  local quote = '"' .. quotes.getQuote() .. '"'
+  notify(quote, "info", opts)
 end
 
 return M
